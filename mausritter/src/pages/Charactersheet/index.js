cb01de84091248595f1bf196cb169d8553c40fa2
Itsapon <robintmier@gmail.com>
2021-11-25 15:21:21 +0100

    Selector for sheet works, now t display

--- mausritter/src/pages/Charactersheet/index.js
+++ mausritter/src/pages/Charactersheet/index.js
@@ -5,12 +5,13 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 //component imports
 import { selectToken } from "../../store/user/selectors";
+import { selectSheetByName } from "../../store/sheets/selectors";
 
 //
 /*TO DO
 V- add a 'back' button to return to homepage
 - MAKE A CSS FILE 
-- create a selector to fetch the details of one specific sheet, 
+V- create a selector to fetch the details of one specific sheet, 
   the one which has the name of the button pressed => edit url to contain name, then params?
 - display the data. 
 - add a button to edit this sheet.
@@ -25,7 +26,9 @@ export default function Charactersheet() {
   }
   const params = useParams();
   const charName = params.name;
+  const sheet = useSelector(selectSheetByName(charName));
   console.log("character name is: ", charName);
+  console.log("sheet is: ", sheet);
   return (
     <div>
       <div>
--- mausritter/src/store/sheets/selectors.js
+++ mausritter/src/store/sheets/selectors.js
@@ -1,1 +1,10 @@
 export const selectUserFull = (state) => state.sheets.userFull;
+
+//get one specific sheet
+
+export const selectSheetByName = (sheetName) => (reduxState) => {
+  const sheet = reduxState.sheets.userFull.sheets.find(
+    (sheet) => sheet.charName === sheetName
+  );
+  return sheet;
+};
